80156510c236eddbd34c5a9b2fd879817891390b
bahaa-alden <samerabood195@gmail.com>
2022-09-26 11:40:41 +0300

    saving to github

--- main.js
+++ main.js
@@ -8,95 +8,18 @@ let winner = document.querySelector(".winner");
 let winh = document.querySelector(".winner h2");
 let replay = document.querySelector(".replay");
 let user = document.querySelector(".user");
+let mode = document.querySelector(".mode");
+let pp = document.querySelector(".pvp");
+let pb = document.querySelector(".pvb");
+let pvb = false;
 let scores = {
   X: 1,
   O: -1,
   tie: 0,
 };
-window.onload = () => {
-  replay.onclick = () => {
-    for (let i = 0; i < allbox.length; i++) {
-      allbox[i].innerHTML = "";
-      allbox[i].style.pointerEvents = "all";
-      allbox[i].removeAttribute("id");
-    }
-    selectbox.classList.toggle("hide");
-    winner.classList.toggle("show");
-    winner.classList.toggle("hide");
-    play.classList.toggle("hide");
-    can = true;
-  };
-  selectXbtn.onclick = function() {
-    selectbox.classList.add("hide");
-    play.classList.add("show");
-    scores.O = 1;
-    scores.X = -1;
-    user.innerHTML = "User : X";
-  };
-  selectObtn.onclick = function () {
-    scores.X = 1;
-    scores.O = -1;
-    user.innerHTML = "User : O";
-
-    selectbox.classList.add("hide");
-    play.classList.add("show");
-    players.setAttribute("class", "players active player");
-  };
-
-  for (let i = 0; i < allbox.length; i++) {
-    allbox[i].setAttribute("onclick", "check(this)");
-  }
-};
 let xIcon = "icon-close";
 let oIcon = "icon-radio-unchecked";
 let can = true;
-function check(element) {
-  if (can) {
-    if (players.classList.contains("player")) {
-      element.innerHTML = `<i class="${oIcon}"></i>`;
-      players.classList.remove("active");
-      element.setAttribute("id", "O");
-    } else {
-      element.innerHTML = `<i class="${xIcon}"></i>`;
-      players.classList.add("active");
-      element.setAttribute("id", "X");
-    }
-    element.style.pointerEvents = "none";
-    can = false;
-  }
-
-  let id = checkWinner();
-
-  if (id === "X" || id === "O") {
-    play.classList.remove("show");
-    play.classList.add("hide");
-    winner.classList.add("show");
-    winh.innerHTML = `Player ${id} won the game!`;
-  } else if (id === "tie") {
-    play.classList.remove("show");
-    play.classList.add("hide");
-    winner.classList.add("show");
-    winh.innerHTML = `No one won the game!`;
-  }
-  setTimeout(() => {
-    bestmove();
-    can = true;
-    let id = checkWinner();
-    if (id === "X" || id === "O") {
-      play.classList.remove("show");
-      play.classList.add("hide");
-      winner.classList.add("show");
-      winh.innerHTML = `Player ${id} won the game!`;
-    } else if (id === "tie") {
-      play.classList.remove("show");
-      play.classList.add("hide");
-      winner.classList.add("show");
-      winh.innerHTML = `No one won the game!`;
-    }
-    allbox.forEach((e) => console.log(e));
-  }, 10);
-}
-
 let cnt2 = 0;
 let board = [];
 let row = [];
@@ -112,7 +35,153 @@ for (let i = 0; i < allbox.length; i++) {
   }
   if (i === allbox.length - 1) board.push(row);
 }
+window.onload = () => {
+  replay.onclick = () => {
+    for (let i = 0; i < allbox.length; i++) {
+      allbox[i].innerHTML = "";
+      allbox[i].style.pointerEvents = "all";
+      allbox[i].removeAttribute("id");
+    }
+    mode.classList.toggle("hide");
+    winner.classList.toggle("show");
+    winner.classList.toggle("hide");
+    play.classList.toggle("hide");
+    can = true;
+    pvb = false;
+    players.classList.remove("player");
+    players.classList.remove("active");
+  };
+  pp.onclick = function () {
+    selectbox.classList.toggle("hide");
+    mode.classList.toggle("hide");
+    for (let i = 0; i < allbox.length; i++) {
+      allbox[i].setAttribute("onclick", "checkpp(this)");
+    }
+  };
+  pb.onclick = function () {
+    selectbox.classList.toggle("hide");
+    mode.classList.toggle("hide");
+    pvb = true;
+    for (let i = 0; i < allbox.length; i++) {
+      allbox[i].setAttribute("onclick", "check(this)");
+    }
+  };
+  selectXbtn.onclick = function () {
+    // can = true;
+    selectbox.classList.toggle("hide");
+    play.classList.add("show");
+    if (pvb) {
+      scores.O = 1; // in pvb mode to let ai be O
+      scores.X = -1;
+      user.innerHTML = "User : X";
+    }
+  };
+  selectObtn.onclick = function () {
+    user.innerHTML = "User : O";
+    if (!pvb) {
+      can = false; //to let the O go first
+    } else {
+      scores.X = 1; // in pvb mode to let ai be X
+      scores.O = -1;
+    }
+    selectbox.classList.add("hide");
+    play.classList.add("show");
+    players.setAttribute("class", "players active player");
+    // go();
+  };
+};
 
+// function go() {
+//   bestmove();
+//   can = true;
+// }
+
+//for P v P
+function checkpp(element) {
+  if (can) {
+    //to add X
+    element.innerHTML = `<i class="${xIcon}"></i>`;
+    players.classList.add("active");
+    element.setAttribute("id", "X");
+    can = false;
+  } //to add O
+  else {
+    element.innerHTML = `<i class="${oIcon}"></i>`;
+    players.classList.remove("active");
+    element.setAttribute("id", "O");
+    can = true;
+  }
+  let id = checkWinner(); //to know if there is a winner
+
+  if (id === "X" || id === "O") {
+    //there is a winner
+    play.classList.remove("show");
+    play.classList.add("hide");
+    winner.classList.add("show");
+    winh.innerHTML = `Player ${id} won the game!`;
+  } else if (id === "tie") {
+    play.classList.remove("show");
+    play.classList.add("hide");
+    winner.classList.add("show");
+    winh.innerHTML = `No one won the game!`;
+  }
+}
+
+//For PvB
+function check(element) {
+  if (can) {
+    if (players.classList.contains("player")) {
+      //if human select O
+      element.innerHTML = `<i class="${oIcon}"></i>`;
+      players.classList.remove("active");
+      element.setAttribute("id", "O");
+    } else {
+      //if human select X
+      element.innerHTML = `<i class="${xIcon}"></i>`;
+      players.classList.add("active"); //to move the blue slider
+      element.setAttribute("id", "X"); //set the chosen sign to use it in check fun
+    }
+    element.style.pointerEvents = "none"; //hide from clicking
+    can = false;
+  }
+
+  let id = checkWinner(); //to know if there is a winner
+
+  if (id === "X" || id === "O") {
+    //there is a winner
+    play.classList.remove("show");
+    play.classList.add("hide");
+    winner.classList.add("show");
+    winh.innerHTML = `Player ${id} won the game!`;
+  } else if (id === "tie") {
+    play.classList.remove("show");
+    play.classList.add("hide");
+    winner.classList.add("show");
+    winh.innerHTML = `No one won the game!`;
+  }
+  // if mode player vs mode
+  if (pvb) {
+    setTimeout(() => {
+      bestmove();
+      can = true;
+      let id = checkWinner();
+      if (id === "X" || id === "O") {
+        //there is a winner
+        play.classList.remove("show");
+        play.classList.add("hide"); //hide winner view
+        winner.classList.add("show"); //show winner view
+        winh.innerHTML = `Player ${id} won the game!`; //show winner view
+      } else if (id === "tie") {
+        //draw
+        play.classList.remove("show");
+        play.classList.add("hide");
+        winner.classList.add("show");
+        winh.innerHTML = `No one won the game!`;
+      }
+    }, 10);
+  }
+}
+// minimax algorithm for bot move
 function bestmove() {
   let bestScore = -Infinity;
   let move;
@@ -126,11 +195,12 @@ function bestmove() {
           board[i][j].innerHTML = `<i class="${oIcon}"></i>`;
           board[i][j].setAttribute("id", "O");
         }
+        //call the minimax s
         let score = minimax(board, 0, false);
-        board[i][j].innerHTML = "";
-        board[i][j].id = "";
+        board[i][j].innerHTML = ""; //after minmax call to remove changes
+        board[i][j].id = ""; //after minmax call to remove changes
         if (score > bestScore) {
-          bestScore = score;
+          bestScore = score; //get the best status
           move = { i, j };
         }
       }
@@ -155,6 +225,7 @@ function minimax(board, depth, isMax) {
     return scores[result];
   }
   if (isMax) {
+    //bot turn
     let bestScore = -Infinity;
     for (let j = 0; j < 3; j++) {
       for (let i = 0; i < 3; i++) {
@@ -175,6 +246,7 @@ function minimax(board, depth, isMax) {
     }
     return bestScore;
   } else {
+    //human turn
     let bestScore = Infinity;
     for (let j = 0; j < 3; j++) {
       for (let i = 0; i < 3; i++) {
@@ -199,6 +271,7 @@ function minimax(board, depth, isMax) {
 function equals3(a, b, c) {
   return a === b && b === c && a !== "";
 }
+//For checking if there is a winner
 function checkWinner() {
   let winner = null;
   for (let i = 0; i < 3; i++) {
